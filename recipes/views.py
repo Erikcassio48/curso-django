8f37fb56c08210d21ce7e9a5d2b323b995540908
Erik Cássio <erikcassio@hotmail.com>
2022-11-27 00:17:15 -0300

    first commit

--- recipes/views.py
+++ recipes/views.py
@@ -6,4 +6,12 @@ def home(request):
     return render(request, 'recipes\pages\home.html', context={
         'name': 'Luiz Otávio',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Luiz Otávio',
+    })
+
+
 # Create your views here.
